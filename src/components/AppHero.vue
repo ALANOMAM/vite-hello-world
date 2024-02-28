3a526401e0c43673f699217731513f7c70004e0a
alan_omam <omamalan@hotmail.it>
2024-02-28 15:50:01 +0100

    SUPER BONUS AGGIUNTO(messo un contattore che fa aumentare il numero ogni volta che clicco)

--- src/components/AppHero.vue
+++ src/components/AppHero.vue
@@ -1,7 +1,20 @@
 
 <script>
 export default{
-    name:"AppHero"
+    name:"AppHero",
+
+    data(){
+        return{
+            nome:"ALAN",
+            conto:0
+        }
+    },
+
+    methods:{
+     countIncrease(){
+        this.conto++
+     }
+    }
 }
 
 </script>
@@ -9,7 +22,7 @@ export default{
 <template>
 
 <div class="jumbotron">
-<h1>HELLO MONDO!</h1>
+<h1>HELLO {{ nome }}</h1>
 
 <P>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
      Architecto,dolorum ullam mollitia ipsum quibusdam porro 
@@ -20,7 +33,8 @@ export default{
 <p class="p2">Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
     Dignissimos, voluptatem?</p>
 
-  <button>Clicca</button>  
+  <button @click="countIncrease()">Count ++</button> 
+  <span>{{ conto }}</span> 
 </div>
 
 </template>
@@ -33,6 +47,13 @@ export default{
     background-color: rgb(212, 206, 206);
 }
 
+span{
+    margin-left: 20px;
+    padding: 5px;
+    background-color: black;
+    color:gold;
+
+}
 button:active{
     background-color: lightpink;
     color: white;
